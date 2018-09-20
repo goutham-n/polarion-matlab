f97918a8ffbe04909967aeb0ea3e7e6e6fb15203
goutham-n <goutham.n@gmail.com>
2018-09-20 17:27:23 +0530

    updated @wi

--- dft.m
+++ dft.m
@@ -1,9 +1,9 @@
 %
 %  Discrete Fourier Transform
-% @ APPL-422 optional comment
+% @wi APPL-422 optional comment
 function Xk = dft(x)
 [N,M] = size(x);
-if M ~=1,   % @ APPL-423 makes sure that x is a column vector
+if M ~=1,   % @wi APPL-423 makes sure that x is a column vector
   x = x';
   N = M;
 end
