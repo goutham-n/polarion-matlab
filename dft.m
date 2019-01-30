52b7a102547b875de97beb61adeb739f6e3327e8
goutham-n <goutham.n@gmail.com>
2019-01-30 12:04:38 +0530

    up

--- dft.m
+++ dft.m
@@ -1,15 +1,15 @@
 
 %  Discrete Fourier Transform
-% @wi APPL-422 optional comment 
+% @wi DP-476 optional comment 
 function Xk = dft(x) 
 [N,M] = size(x);
-if M ~=1,   % @wi APPL-423 makes sure that x is a column vector
+if M ~=1,   % @wi DP-478 makes sure that x is a column vector
   x = x';
   N = M;
 end
 Xk=zeros(N,1);
 n = 0:N-1;
-% @wi APPL-425 for loop
+% @wi DP-528 for loop
 for k=0:N-1
   Xk(k+1) = exp(-j*2*pi*k*n/N)*x;
 end
